4c6958d669cd1a875c682ad50ccc47a19a0538f2
Fernanda <143466223+Sunref@users.noreply.github.com>
2025-08-17 11:28:21 -0300

    Update config.h

--- niteProject/include/config.h
+++ niteProject/include/config.h
@@ -5,7 +5,7 @@
 #define TITLE "NITE"
 #define SUBTITLE "NANO-INSPIRED TEXT EDITOR"
 #define VERSION "ver. 0.01"
-#define AUTHOR "Created by Fernanda M."
+#define AUTHOR "Criado por Fernanda M."
 
 // Cores
 #define COLOR_O COLOR_BLUE
