101c9b7a76def99e935fdf8bee435698f8f20017
Fernanda <143466223+Sunref@users.noreply.github.com>
2025-09-13 12:59:48 -0300

    NEW: Preparing to implement the editor
    (just an initial idea, zero implementations)

---
+++ niteProject/include/editor.h
@@ -0,0 +1,3 @@
+// TODO:
+// IMPLEMENTAR -> Criar novo arquivo; Abrir arquivo em modo leitura e disponibilizar modo edição;
+//  Salvar arquivo (novo e aberto); Fechar Arquivo; Sair do arquivo;
---
+++ niteProject/src/editor.c
@@ -0,0 +1,4 @@
+// TODO:
+// IMPLEMENTAR -> Criar novo arquivo; Abrir arquivo em modo leitura e disponibilizar modo edição;
+//  Salvar arquivo (novo e aberto); Fechar Arquivo; Sair do arquivo;
+
